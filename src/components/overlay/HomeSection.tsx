b2fc99ae08946671bebe77ec6ac032748c7ca74b
razikdontcare <gamersia4@gmail.com>
2024-10-31 03:38:58 +0800

    Refactor background video handling and move HomeSection to components

--- src/components/overlay/HomeSection.tsx
+++ src/components/overlay/HomeSection.tsx
@@ -1,10 +1,10 @@
 export default function HomeSection() {
   return (
     <>
-      <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full  pointer-events-none">
+      <div className="flex items-center justify-center absolute top-0 left-0 w-full h-full pointer-events-none">
         <div className="text-center">
           <h1 className="text-6xl font-bold text-white cursor-default pointer-events-auto">
-            Overlay
+            bismillah gak ngebug
           </h1>
         </div>
       </div>
