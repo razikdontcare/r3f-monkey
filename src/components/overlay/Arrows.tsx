7ad87923e912a8e1ac8c6f1684fdcf1054cbdce6
razikdontcare <gamersia4@gmail.com>
2024-11-22 20:38:44 +0800

    Refactor Arrows and UIOverlay components for improved button functionality and styling

--- src/components/overlay/Arrows.tsx
+++ src/components/overlay/Arrows.tsx
@@ -13,7 +13,7 @@ export default function Arrows() {
     e.preventDefault();
     setTargetPosition(
       targetPosition[0] - SCENE_STEP < 0
-        ? [0, 0, 0]
+        ? [MAX_POSITION, 0, 0]
         : [targetPosition[0] - SCENE_STEP, 0, 0]
     );
   };
@@ -22,7 +22,7 @@ export default function Arrows() {
     e.preventDefault();
     setTargetPosition(
       targetPosition[0] + SCENE_STEP > MAX_POSITION
-        ? [MAX_POSITION, 0, 0]
+        ? [0, 0, 0]
         : [targetPosition[0] + SCENE_STEP, 0, 0]
     );
   };
@@ -31,32 +31,10 @@ export default function Arrows() {
     <>
       <div className="w-full mx-auto flex items-center justify-center absolute top-0 left-0 h-full pointer-events-none">
         <div className="w-full mx-auto flex flex-col items-center justify-center text-center gap-10">
-          {/* <h1 className="text-6xl font-bold text-white cursor-default pointer-events-auto">
-            bismillah gak ngebug
-          </h1> */}
-          <div className="w-full mx-auto pl-[34rem] pr-10 flex items-center justify-between gap-5 text-white pointer-events-none">
-            <button
-              onClick={handlePrev}
-              className="bg-rose-800 hover:bg-rose-700 disabled:bg-rose-900 rounded p-2 pointer-events-auto"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 19.5 8.25 12l7.5-7.5"
-                />
-              </svg>
-            </button>
+          <div className="w-full mx-auto pr-4 flex items-center justify-end gap-5 text-white pointer-events-none">
             <button
               onClick={handleNext}
-              className="bg-rose-800 hover:bg-rose-700 disabled:bg-rose-900 rounded p-2 pointer-events-auto"
+              className="bg-[#b98c63] hover:bg-[#cc9c73] disabled:bg-[#946b54] rounded p-2 pointer-events-auto"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
