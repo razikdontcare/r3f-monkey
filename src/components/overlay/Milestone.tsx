662b0eb77451b987d701d346554db7bcabc99cd8
razikdontcare <gamersia4@gmail.com>
2024-11-22 20:08:25 +0800

    Refactor Milestones component layout for improved button positioning and responsiveness

--- src/components/overlay/Milestone.tsx
+++ src/components/overlay/Milestone.tsx
@@ -7,33 +7,35 @@ export default function Milestones() {
   return (
     <>
       <div className="w-full mx-auto flex items-center justify-center absolute top-0 left-0 h-full pointer-events-none">
-        <div className="absolute w-fit top-10">
-          <Image src={milestones} alt="MILESTONES" className={`size-full`} />
-          <button
-            id="prehistoric"
-            className="size-12 pointer-events-auto absolute top-1 left-1 rounded-full"
-            onClick={() => setTargetPosition([0, 0, 0])}
-          />
-          <button
-            id="egypt"
-            className="size-12 pointer-events-auto absolute top-1 left-[9.8rem] rounded-full"
-            onClick={() => setTargetPosition([10, 0, 0])}
-          />
-          <button
-            id="dynasty"
-            className="size-12 pointer-events-auto absolute top-1 left-[19.45rem] rounded-full"
-            onClick={() => setTargetPosition([20, 0, 0])}
-          />
-          <button
-            id="ww2"
-            className="size-12 pointer-events-auto absolute top-1 right-[9.8rem] rounded-full"
-            onClick={() => setTargetPosition([30, 0, 0])}
-          />
-          <button
-            id="nyc"
-            className="size-12 pointer-events-auto absolute top-1 right-1 rounded-full"
-            onClick={() => setTargetPosition([40, 0, 0])}
-          />
+        <div className="absolute top-10  size-fit">
+          <div className="relative">
+            <Image src={milestones} alt="MILESTONES" className="w-[35rem]" />
+            <button
+              id="prehistoric"
+              className="size-12 pointer-events-auto absolute top-0 left-0 rounded-full "
+              onClick={() => setTargetPosition([0, 0, 0])}
+            />
+            <button
+              id="egypt"
+              className="size-12 pointer-events-auto absolute top-0 left-32 rounded-full "
+              onClick={() => setTargetPosition([10, 0, 0])}
+            />
+            <button
+              id="dynasty"
+              className="size-12 pointer-events-auto absolute top-0 left-64 rounded-full "
+              onClick={() => setTargetPosition([20, 0, 0])}
+            />
+            <button
+              id="ww2"
+              className="size-12 pointer-events-auto absolute top-0 right-32 rounded-full "
+              onClick={() => setTargetPosition([30, 0, 0])}
+            />
+            <button
+              id="nyc"
+              className="size-12 pointer-events-auto absolute top-0 right-0 rounded-full "
+              onClick={() => setTargetPosition([40, 0, 0])}
+            />
+          </div>
         </div>
       </div>
     </>
