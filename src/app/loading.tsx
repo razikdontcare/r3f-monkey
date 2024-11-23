a06d5243d1f96299f758e2d68da1ef1283eafbda
razikdontcare <gamersia4@gmail.com>
2024-11-24 01:30:30 +0800

    Comment out LoadingAnimation component in loading.tsx for potential future use

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,12 @@
 export const dynamic = "force-dynamic";
 
-import { LoadingAnimation } from "@/components/animation";
+// import { LoadingAnimation } from "@/components/animation";
 
 export default function Loading() {
   return (
     <>
       <div className="h-screen flex flex-col items-center justify-center w-full bg-neutral-800">
-        <LoadingAnimation />
+        {/* <LoadingAnimation /> */}
       </div>
     </>
   );
