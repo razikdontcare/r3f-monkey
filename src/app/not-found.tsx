a23e744c2c9a914b374b1ca55b2333faff25f1a8
razikdontcare <gamersia4@gmail.com>
2024-11-24 01:25:05 +0800

    Add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="h-screen flex flex-col items-center justify-center w-full bg-neutral-800">
+        <h1 className="text-4xl font-bold text-white">404</h1>
+        <p className="text-white">Page not found</p>
+      </div>
+    </>
+  );
+}
