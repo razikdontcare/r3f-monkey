e14933042aa1948efdc8a14b08bb4c7e17c6e660
razikdontcare <gamersia4@gmail.com>
2024-11-24 00:24:07 +0800

    Implement custom 404 Not Found page workaround by creating not-found.ts and notfound-component.tsx

---
+++ src/app/notfound-component.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Error from "next/error";
+
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <Error statusCode={404} />
+      </body>
+    </html>
+  );
+}
