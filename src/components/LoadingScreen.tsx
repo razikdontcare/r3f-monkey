ed49b23cb7f06cfa9bda5abe950ef6c52cb1b922
Ricky <ricky.georgius@gmail.com>
2024-11-25 13:02:53 +0700

    fix: hide load finish

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -18,8 +18,9 @@ export default function LoadingScreen() {
       <div className="text-white text-2xl">Loading {progress}%</div>
     </div>
   ) : (
-    <div className="h-screen w-screen flex absolute z-50 top-0 left-0 items-center justify-center bg-black/10">
-      <div className="text-white text-2xl">Loading Finished</div>
-    </div>
+    <></>
+    // <div className="h-screen w-screen flex absolute z-50 top-0 left-0 items-center justify-center bg-black/10">
+    //   <div className="text-white text-2xl">Loading Finished</div>
+    // </div>
   );
 }
