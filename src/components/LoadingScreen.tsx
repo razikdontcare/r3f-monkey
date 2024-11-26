0dc0ad74bb8c1d209ab19ac9171881c35b667b88
Toton Dwi Antoko <Totonantoko@gmail.com>
2024-11-26 23:45:34 +0700

    feat: background paralax sequent animation

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,10 +3,11 @@
 
 import { useProgress } from "@react-three/drei";
 import { useState, useEffect } from "react";
+import { useThree } from '@react-three/fiber';
 import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { useTexture } from '@react-three/drei';
-import { SpriteMaterial, LinearFilter, NoToneMapping, Sprite, Object3DEventMap } from 'three';
+import { SpriteMaterial, LinearFilter, NoToneMapping, Sprite, Object3DEventMap, Mesh, BufferGeometry, NormalBufferAttributes, Material, Texture } from 'three';
 
 export default function LoadingScreen() {
   const [loading, setLoading] = useState(true);
@@ -14,7 +15,14 @@ export default function LoadingScreen() {
   const [confirmation, setConfirmation] = useState<'yes' | 'no'>()
   const { progress } = useProgress();
 
+  const [lastMousePosition, setLastMousePosition] = useState([0, 0]); // Track last mouse position
+  const [mouseIdle, setMouseIdle] = useState(false); // Track if mouse is idle
 
+  // Handle mouse movement
+  const handleMouseMove = (event: any) => {
+    setLastMousePosition([event.clientX, event.clientY]);
+    setMouseIdle(false);
+  };
 
   useEffect(() => {
     if (confirmation === 'yes') {
@@ -34,6 +42,11 @@ export default function LoadingScreen() {
 
   return !openApp ? (
     <div className="h-screen w-screen flex absolute z-50 top-0 left-0 items-center justify-center bg-black">
+      <div className="h-full w-full overflow-hidden absolute inset-0">
+        <Canvas onMouseMove={handleMouseMove}>
+          <ParallaxScene lastMousePosition={lastMousePosition} mouseIdle={mouseIdle} setMouseIdle={setMouseIdle} />
+        </Canvas>
+      </div>
 
       {/* Bar Screen */}
       <div className="absolute left-20 bottom-10 text-center w-[15%]">
@@ -153,4 +166,180 @@ const SpriteAnimation = ({ confirmation }: { confirmation: 'yes' | 'no' | undefi
   );
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ParallaxPlane = ({ image, depth, scale, position, speed, opacity, direction }: {
+  image: Texture
+  depth: number
+  scale: any
+  position: any
+  speed: number
+  opacity: number
+  direction: number
+}) => {
+  const ref = useRef<Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>>(null);
+  // const { size } = useThree();
+  // const aspectRatio = size.width / size.height;
+  const targetPosition = useRef([0, 0, 0]);
 
+  // Update position based on mouse movement with lerp for smoothness
+  useFrame(({ mouse }) => {
+    const targetX = (mouse.x * depth) / 2;
+    const targetY = (mouse.y * depth) / 2;
+
+    // Lerp towards the target position for smooth movement
+    targetPosition.current[0] += (targetX - targetPosition.current[0]) * speed; /*speed=0.1*/
+    targetPosition.current[1] += (targetY - targetPosition.current[1]) * speed;
+
+    ref.current?.position.set(targetPosition.current[0], targetPosition.current[1], 0);
+  });
+
+  // Update position with mouse or automatic movement
+  useFrame(() => {
+    targetPosition.current[1] += direction * speed; // Move up or down based on direction
+
+    // Reset position when it goes out of view to create an infinite scroll effect
+    if (targetPosition.current[1] > 1 || targetPosition.current[1] < -1) {
+      targetPosition.current[1] = direction > 0 ? -1 : 1;
+    }
+
+    ref.current?.position.set(targetPosition.current[0], targetPosition.current[1], 0);
+  });
+
+
+  return (
+    <mesh ref={ref} position={position} scale={scale}>
+      <planeGeometry args={[1, 1]} />
+      <meshBasicMaterial map={image} transparent={true} opacity={opacity} />
+      {/* alphaTest={0} */}
+    </mesh>
+  );
+}
+
+const ParallaxScene = ({ lastMousePosition, mouseIdle, setMouseIdle }: {
+  lastMousePosition: number[]
+  mouseIdle: boolean
+  setMouseIdle: (value: boolean) => void
+}) => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [opacity, setOpacity] = useState(1);
+  // const [fadeDirection, setFadeDirection] = useState('out');
+
+  const backgroundTexture = [
+    useTexture('/preloader/preloader-images/first-Sequence/adams-creation/background.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/austrian-painter/background.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/battle-of-hu-lao-gate-2/background.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/boxing/background.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/moses/background.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/three-kingdom/background.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/abraham/background.jpeg'),
+    useTexture('/preloader/preloader-images/second-Sequence/adam-eve/background.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/leonardo/background.jpg'),
+    useTexture('/preloader/preloader-images/second-Sequence/mlk/background.png')
+  ];
+  const characterTexture = [
+    useTexture('/preloader/preloader-images/first-Sequence/adams-creation/character.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/austrian-painter/character.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/battle-of-hu-lao-gate-2/character.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/boxing/character.png'),
+    useTexture('/preloader/preloader-images/first-Sequence/moses/character.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/three-kingdom/character.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/abraham/character.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/adam-eve/character.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/leonardo/character.png'),
+    useTexture('/preloader/preloader-images/second-Sequence/mlk/character.png')
+  ];
+
+  // Get the size of the canvas
+  const { width, height } = useThree().size;
+
+  // Update scale and position to fill the full screen for the background
+  const backgroundScale = [width / 100, height / 100, 1];
+  const characterScale = [width / 100, height / 100, 1]; // Set the desired scale for the character
+
+  const speed = 0.1; // Speed for smooth movement (lower value = smoother)
+
+  useEffect(() => {
+    const fadeOut = () => {
+      // setFadeDirection('out');
+      let currentOpacity = 1;
+      const fadeOutInterval = setInterval(() => {
+        if (currentOpacity > 0) {
+          currentOpacity -= 0.05; // Decrease opacity for fade-out effect
+          setOpacity(currentOpacity);
+        } else {
+          clearInterval(fadeOutInterval); // Stop fade-out when opacity is 0
+        }
+      }, 50);
+    };
+
+    const fadeIn = () => {
+      // setFadeDirection('in');
+      let currentOpacity = 0;
+      const fadeInInterval = setInterval(() => {
+        if (currentOpacity < 1) {
+          currentOpacity += 0.05; // Increase opacity for fade-in effect
+          setOpacity(currentOpacity);
+        } else {
+          clearInterval(fadeInInterval); // Stop fade-in when opacity is 1
+        }
+      }, 50);
+    };
+
+    const interval = setInterval(() => {
+      fadeOut(); // Start fade-out when it's time to change image
+      setTimeout(() => {
+        setImageIndex((prevIndex) => (prevIndex + 1) % 2); // Change image index
+        fadeIn(); // Start fade-in after changing image
+      }, 1000); // Wait for 1 second for fade-out before fading in new image
+    }, 4000); // Change images every 4 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+
+  // Set idle after 2 seconds of inactivity
+  useEffect(() => {
+    const idleTimeout = setTimeout(() => {
+      setMouseIdle(true);
+    }, 2000); // After 2 seconds of no mouse movement, mark as idle
+
+    return () => clearTimeout(idleTimeout);
+  }, [lastMousePosition]);
+
+  const getParallaxDirection = () => {
+    if (mouseIdle) {
+      return [0.01, -0.01]; // Automatic movement when idle
+    } else {
+      const [mouseX, mouseY] = lastMousePosition;
+      const xOffset = (mouseX / window.innerWidth) * 2 - 1;
+      const yOffset = (mouseY / window.innerHeight) * 2 - 1;
+      return [xOffset * 0.05, yOffset * 0.05]; // Mouse-based parallax movement
+    }
+  };
+
+  const [backgroundDirection, characterDirection] = getParallaxDirection();
+
+  return (
+    <>
+      <ParallaxPlane
+        image={backgroundTexture[imageIndex]}
+        depth={0.1}
+        scale={backgroundScale}
+        position={[0, 0, -1]}
+        // transparent={false}
+        speed={speed}
+        opacity={opacity}
+        direction={backgroundDirection}
+      />
+      <ParallaxPlane
+        image={characterTexture[imageIndex]}
+        depth={0.3}
+        scale={characterScale}
+        position={[0, 0, 0]} // Character stays on top
+        // transparent={true}
+        speed={speed}
+        opacity={opacity}
+        direction={characterDirection}
+      />
+    </>
+  );
+}
